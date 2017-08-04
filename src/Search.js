a52ba70a6ed2aa844a4234808a282ba6731e3066
Marc <phyrenight@gmail.com>
2017-08-04 03:33:13 -0400

    feat: add select input to books

--- src/Search.js
+++ src/Search.js
@@ -28,7 +28,21 @@ class Search extends Component{
           {this.props.books.map((book) => (
             <li key={book.id} >
               <div style={{width: 126, height: 188, backgroundImage: `url(${book.imageLinks.thumbnail})`
-            }}/>
+            }}>
+                <div className="book">
+                  <div className="book-top">
+                    <div className="book-shelf-changer">
+                      <select>
+                        <option value="none" disabled>Move to...</option>
+                        <option value="currentlyReading">Currently Reading</option>
+                        <option value="wantToRead">Want to Read</option>
+                        <option value="read">Read</option>
+                        <option value="none">None</option>
+                      </select>
+                    </div>
+                  </div>  
+                </div>
+              </div>
             </li>
             ))}
 
